11eb418bcbe6047ef99faa31401a7554d3703a34
swfz <sawafuji.09@gmail.com>
2016-10-13 00:27:08 +0900

    puma

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,8 +2,7 @@
 lock '3.6.1'
 
 set :application, 'sample'
-# set :repo_url, 'git@github.com:swfz/rails-api-sample.git'
-set :repo_url, 'https://github.com/swfz/rails-api-sample.git'
+set :repo_url, 'git://192.168.30.81/sample.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
@@ -28,7 +27,7 @@ set :deploy_to, '/var/www/sample'
 # append :linked_files, 'config/database.yml', 'config/secrets.yml'
 
 # Default value for linked_dirs is []
-# append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
